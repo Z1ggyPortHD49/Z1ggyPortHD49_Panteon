a546a321cf0ba8085bcfe3356aad6ef3ff110828
Z1ggyPortHD49 <krababal28@gmail.com>
2025-01-23 17:32:42 +0300

    123456

--- Ren.java
+++ Ren.java
@@ -22,7 +22,7 @@ public class Ren {
         }
         System.out.println(arrayList + " - Все ямы");
         System.out.println(palCheck(array) + " - Палиндромность");
-        System.out.println(Collections.max(pak) + Collections.max(pak) - 1 + " - Максимальная палиндромная яма");
+        System.out.println(Collections.max(pak) * 2 - 1 + " - Максимальная палиндромная яма");
     }
 
     static boolean palCheck(int[] array) {
